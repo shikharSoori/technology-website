f1738c0dbc88c958183c5969b8aee436f7ca39cb
Shikhar Manandhar <124882005+shikharSoori@users.noreply.github.com>
2024-07-23 14:36:16 +0545

    totaling in trial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,59 @@ import Scroller from "@/components/Scroller/Scroller";
 import TrackTraceVideo from "@/components/TrackTraceVideo/TrackTraceVideo";
 import MotoBanner from "@/components/MotoBanner/MotoBanner";
 import Blog from "@/components/Blog/Blog";
+import Image from "next/image";
+import RfidBanner from "@/assets/BANNERRR.png";
+import Link from "next/link";
+import TitleText from "@/components/Testimonials/TitleText";
 // export const revalidate = 3600;
 const Home = () => {
   return (
     <>
       <HomeHero />
       <MotoBanner />
-      
       <TrackTraceVideo />
       <HomeServices />
+      <Link href="/solutions/trackline">
+        <div
+          className=" rfid-trackline-banner w-100"
+          style={{
+            backgroundImage: `url(${RfidBanner.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderTop: " 1px solid #e7e7e7e",
+          }}
+        >
+          <div className="h-100 container section-padding text-right">
+            <h3 className="h1 title" style={{ fontSize: "70px" }}>
+              <TitleText
+                title={`Build <span> Faster</span> <br/>Automation <br/>  `}
+              />
+              with <span style={{ fontSize: "70px" }}> RFID</span>
+            </h3>
+            <Link href="/solutions/trackline" className="btn btn-all mt-4">
+              Read More
+            </Link>
+          </div>
+        </div>
+      </Link>
       <WhySoori />
+      {/* <Image
+          className="w-100 rfid-banner-home"
+          src={RfidBanner}
+          style={{
+            objectFit: "cover",
+            height: "400px",
+            objectPosition: "center",
+            cursor: "pointer",
+          }}
+          alt="banner"
+          width={1000}
+          height={200}
+        /> */}
 
       <Blog />
       <Testimonials />
-
       <Partners />
-
       <Scroller />
     </>
   );
